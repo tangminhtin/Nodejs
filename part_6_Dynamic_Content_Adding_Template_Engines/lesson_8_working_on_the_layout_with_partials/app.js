4ae8bef1fe14c13cf1f1c84c4bb9411472e4a5cb
tangminhtin <minhtintang@gmail.com>
2020-08-13 22:55:32 +0700

    Working on the layout with partials

--- part_6_Dynamic_Content_Adding_Template_Engines/lesson_8_working_on_the_layout_with_partials/app.js
+++ part_6_Dynamic_Content_Adding_Template_Engines/lesson_8_working_on_the_layout_with_partials/app.js
@@ -30,7 +30,10 @@ app.use(shopRoutes);
 
 // adding 404 error page
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        path: ''
+    });
 });
 
 // listener request
