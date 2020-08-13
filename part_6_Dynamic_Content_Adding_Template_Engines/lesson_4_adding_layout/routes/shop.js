bd786a4b5a5b672ac2a00d6e4eb7017a7c6cdda4
tangminhtin <minhtintang@gmail.com>
2020-08-13 18:51:01 +0700

    Adding layout, finishing the pug template

---
+++ part_6_Dynamic_Content_Adding_Template_Engines/lesson_4_adding_layout/routes/shop.js
@@ -0,0 +1,26 @@
+// import path
+const path = require('path');
+
+// import express module
+const express = require('express');
+
+// import path of root directory
+const rootDir = require('../util/path');
+
+// import admin data
+const adminData = require('./admin');
+
+// get router
+const router = express.Router();
+
+// listen user type url /
+router.get('/', (req, res, next) => {
+    // get product from admin data
+    const products = adminData.products;
+
+    // render shop.pug page, passing products data into shop.pug page
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+});
+
+// export router
+module.exports = router;
