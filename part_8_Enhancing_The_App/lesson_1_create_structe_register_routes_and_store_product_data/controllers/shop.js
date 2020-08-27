6dacfcb8f06a1d07d306ba49dc8485e4b35fe2e8
tangminhtin <minhtintang@gmail.com>
2020-08-27 11:00:15 +0700

    Creating shop structes, registering the routes and storing product data

---
+++ part_8_Enhancing_The_App/lesson_1_create_structe_register_routes_and_store_product_data/controllers/shop.js
@@ -0,0 +1,44 @@
+// import product model
+const Product = require('../models/product');
+
+// get all products
+exports.getProducts = (req, res, next) => {
+    // fetch all products
+    Product.fetchAll(products => {
+        // render shop.pug page, passing products data into shop.hbs page
+        res.render('shop/product-list', {
+            prods: products, 
+            pageTitle: 'All Products', 
+            path: '/product',
+        });
+    });
+};
+
+// get index
+exports.getIndex = (req, res, next) => {
+     // fetch all products
+     Product.fetchAll(products => {
+        // render shop.pug page, passing products data into shop.hbs page
+        res.render('shop/index', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/',
+        });
+    });
+};
+
+// get cart
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path:'/cart'
+    });
+};
+
+// get checkout
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        pageTitle: 'Checkout',
+        path: '/checkout'
+    });
+};
---
+++ part_8_Enhancing_The_App/lesson_1_create_structe_register_routes_and_store_product_data/models/product.js
@@ -0,0 +1,50 @@
+// import file system and path
+const fs = require('fs');
+const path = require('path');
+
+// name and location of file
+const p = path.join(
+    path.dirname(process.mainModule.filename), 
+    'data', 
+    'products.json'
+);
+
+// read data from file
+const getProductsFromFile = cb => {
+    // read data from json file
+    fs.readFile(p, (err, fileContent) => {
+        if(err) {
+            cb([]);
+        } else {
+            // convert json to js
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
+
+module.exports = class Product {
+    constructor(title, imageURL, description, price) {
+        this.title = title;
+        this.imageURL = imageURL;
+        this.description = description;
+        this.price = price;
+    }
+
+    // save object file
+    save() {
+        getProductsFromFile(products => {
+            // put data into array
+            products.push(this);
+
+            // converts js to json and write to file
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    // get all data
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
+    }
+};
