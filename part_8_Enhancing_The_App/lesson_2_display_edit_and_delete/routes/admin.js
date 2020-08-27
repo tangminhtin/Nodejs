937ab2da78426ab18882ef9d8b6fde8a36000c4f
tangminhtin <minhtintang@gmail.com>
2020-08-27 23:41:25 +0700

    Display, edit, delete and order product

---
+++ part_8_Enhancing_The_App/lesson_2_display_edit_and_delete/routes/admin.js
@@ -0,0 +1,23 @@
+// import path
+const path = require('path');
+
+// import express module
+const express = require('express');
+
+// import admin controller
+const adminController = require('../controllers/admin');
+
+// get router
+const router = express.Router();
+
+// /admin/add-product => GET // listen user type url add-product
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST // handler add-product
+router.post('/add-product', adminController.postAddProduct);
+
+// export router
+module.exports= router;
