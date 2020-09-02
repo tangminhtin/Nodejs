97e82a3ed3357a154556abf43fd63ce89a46a3e0
tangminhtin <minhtintang@gmail.com>
2020-09-02 23:24:02 +0700

    Connect to database

--- part_11_Sequelize/lesson_1_connection_database/util/database.js
+++ part_11_Sequelize/lesson_1_connection_database/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('online-shop', 'root', '', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'online-shop',
-    password: ''
-})
+});
 
-module.exports = pool.promise();
+module.exports = sequelize;
