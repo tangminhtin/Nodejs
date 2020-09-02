ee9827e23b08f9ae9f0a8f019c831806bd98d227
tangminhtin <minhtintang@gmail.com>
2020-09-02 09:50:40 +0700

    add delete route, delete cart item, delete product

---
+++ part_9_Dynamic_Routes/lesson_3_delete_product/controllers/shop.js
@@ -0,0 +1,78 @@
+// import product model
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+// get all products
+exports.getProducts = (req, res, next) => {
+    // fetch all products
+    Product.fetchAll(products => {
+        // render shop.pug page, passing products data into shop.hbs page
+        res.render('shop/product-list', {
+            prods: products, 
+            pageTitle: 'All Products', 
+            path: '/product',
+        });
+    });
+};
+
+// get product detail
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: "Product Detail",
+            path: "/product"
+        });
+    });
+}
+
+// get index
+exports.getIndex = (req, res, next) => {
+     // fetch all products
+     Product.fetchAll(products => {
+        // render shop.pug page, passing products data into shop.hbs page
+        res.render('shop/index', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/',
+        });
+    });
+};
+
+// get cart
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path:'/cart'
+    });
+};
+
+// add to cart
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    })
+    res.redirect('/');
+    // res.render('shop/cart', {
+    //     pageTitle: 'Your Cart',
+    //     path:'/cart'
+    // });
+};
+
+// get orders
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        pageTitle: 'Your Order',
+        path: '/orders'
+    });
+}
+
+// get checkout
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        pageTitle: 'Checkout',
+        path: '/checkout'
+    });
+};
---
+++ part_9_Dynamic_Routes/lesson_3_delete_product/models/cart.js
@@ -0,0 +1,73 @@
+// import file system and path
+const fs = require('fs');
+const path = require('path');
+
+// name and location file
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    // add new product into cart
+    static addProduct(id, productPrice) {
+        // fetch the previous cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = { products: [], totalPrice: 0 }
+            // if cart.json existed, then convert json to object js
+            if(!err) {
+                // get data from file and convert to objet
+                cart = JSON.parse(fileContent);
+            }
+
+            // analyze the cart => find existing product
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+
+            // add new product / increase quantity
+            if(existingProduct) {
+                // get current product object and store it into update product
+                updatedProduct = {...existingProduct}
+                updatedProduct.qty = updatedProduct.qty + 1;
+                // get old products array of cart and store update product into cart
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex] = updatedProduct;
+            } else {
+                // add new product object into cart
+                updatedProduct = {id: id, qty: 1};
+                // get old product and add new product into products array of cart
+                cart.products = [...cart.products, updatedProduct];
+            }
+
+            // update total price of cart
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            // write cart into json file
+            fs.writeFile(p, JSON.stringify(cart), (err) => {
+                console.log(err);
+            });
+        });
+    }
+
+    // delete product in cart
+    static deleteProduct(id, productPrice) {
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return;
+            }
+            const updatedCart = {...JSON.parse(fileContent)};
+            const product = updatedCart.products.find(prod => prod.id === id);
+            const productQty = product.qty;
+            updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+            updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+        
+            fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+                console.log(err);
+            });
+        });
+    }
+};
+
+
+
