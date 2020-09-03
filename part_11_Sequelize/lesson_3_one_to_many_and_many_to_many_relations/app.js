0b8b2bbe4c17803150c286901f6b2c7bcd40c152
tangminhtin <minhtintang@gmail.com>
2020-09-04 00:01:44 +0700

    Adding order, order item model

--- part_11_Sequelize/lesson_3_one_to_many_and_many_to_many_relations/app.js
+++ part_11_Sequelize/lesson_3_one_to_many_and_many_to_many_relations/app.js
@@ -13,11 +13,13 @@ const errorController = require('./controllers/error');
 // import sequelize
 const sequelize = require('./util/database');
 
-// import product, user, cart, cart item model
+// import product, user, cart, cart item, order, order item model
 const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 // create an application
 const app = express();
@@ -64,6 +66,10 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
 
 // create tables in database
 // sequelize.sync({force: true}) // overwrite database
