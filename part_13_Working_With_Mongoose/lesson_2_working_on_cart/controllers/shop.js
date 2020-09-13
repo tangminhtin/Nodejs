3006e9c41f6ad4cf0dd5c56891065ce3c6bfa7da
tangminhtin <minhtintang@gmail.com>
2020-09-13 12:28:15 +0700

    Getting displaying the orders

--- part_13_Working_With_Mongoose/lesson_2_working_on_cart/controllers/shop.js
+++ part_13_Working_With_Mongoose/lesson_2_working_on_cart/controllers/shop.js
@@ -120,9 +120,7 @@ exports.postOrder = (req, res, next) => {
 
 // get orders
 exports.getOrders = (req, res, next) => {
-    req.user
-        // fetch products table
-        .getOrders()
+    Order.find({'user.userId': req.user._id})
         .then((orders) => {
             res.render('shop/orders', {
                 pageTitle: 'Your Order',
